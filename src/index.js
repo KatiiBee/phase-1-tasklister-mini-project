381bbe2d75820ffc1a0f0a94ba0dacce47a2ebed
Katie Demetriff <kdemetriff@gmail.com>
2022-09-25 07:17:16 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  let text= document.querySelector('#new-task-description')
+  let tasks= document.querySelector('#tasks')
+  form.addEventListener ('submit', makeNewTask)
+  function makeNewTask(event){
+    event.preventDefault()
+    let li = document.createElement('li')
+    let btn= document.createElement('button')
+    li.textContent = text.value
+    btn.textContent = 'x'
+    btn.addEventListener('click', deleteButton)
+    tasks.appendChild(li)
+    li.appendChild (btn)
+
+  }
+function deleteButton (event){
+  event.target.parentNode.remove()
+}
 });
